1f7ab5add7d50ae590e9f1e597088447b7abaa12
Ivan <pl_st@list.ru>
2022-12-07 01:29:12 +0300

    refactor: todoList feat: added todo conditional rendering

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <the-header/>
+    <todo-list/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import TodoList from "@/components/TodoList/index.vue";
+import TheHeader from "@/components/TheHeader.vue";
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    TodoList,
+    TheHeader
   }
 }
 </script>
